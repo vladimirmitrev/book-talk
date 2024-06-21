8ed89edb1f1e1d12bca46ab2babfdc6d189b5e6e
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-06-21 12:57:22 +0300

    Add view profile

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,10 +1,20 @@
 const router = require('express').Router();
 const { isAuth } = require('../middlewares/authMiddleware');
+const userService = require('../services/userService');
+
 
 router.get('/', async (req, res) => {
 
   res.render('home');
 });
 
+router.get('/profile', isAuth, async (req, res) => {
+
+  const userId = req.user._id;
+
+  const user = await userService.getInfo(userId).lean();
+
+  res.render('profile', {user});
+});
 
 module.exports = router;
